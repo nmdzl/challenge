72a782f62957c92f492d7cec64b22822282111e4
nmdzl <zding8@ncsu.edu>
2021-08-24 13:22:13 -0400

    configuration

---
+++ src/client/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App () {
+    return (
+        <div>
+            <h1>All Users</h1>
+            <h4>Users and their age</h4>
+        </div>
+    );
+}
+
+export default App;
